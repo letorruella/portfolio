daddac26a082c3efee0add37d6000583a5c42aa8
Luis Torruella <letorruella@gmail.com>
2019-03-05 18:03:48 -0500

    CSS: styled portfolio section

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -1,28 +1,30 @@
-
 import PropTypes from "prop-types"
 import React from "react"
+import neighborhood from "../images//neighborhood.png"
 
-const Portfolio = ({ siteTitle }) => (
-  <portfolio class="portfolio"> 
-  <hr/>
-   <section class="portfolio_container">
-    <div class="portfolio_left">
-      <h1>-Neightborhood</h1>
-      <p>Apparently we had reached a great height in the atmosphere, 
-        for the sky was a dead black, and the stars had ceased to twinkle. 
-        By the same illusion which lifts
-        the horizon of the sea to the level of the spectator on a hillside.
-      </p>
-      <div class="tags">
-        <a>knockout</a>
-        <a>source</a>
-      </div>  
-    </div>
-    <div class="portfolio_right">
-      <div class="img-container"></div>
-    </div>
-  </section>
-   
+const Portfolio = ({ siteTitle, portfolio01 }) => (
+  <portfolio class="portfolio">
+    <section class="portfolio_container">
+      <div class="portfolio_left">
+        <h1>{portfolio01.title}</h1>
+        
+        <p>
+         {portfolio01.desc}
+        </p>
+       <a href={portfolio01.source}>source.</a>
+        <div class="tags">
+       <br/>
+        {portfolio01.tags.map((t)=>(
+
+          <a class>{t}</a>
+        ))}
+
+        </div>
+      </div>
+      <div class="portfolio_right">
+        <div class="img-container" />
+      </div>
+    </section>
   </portfolio>
 )
 
@@ -32,6 +34,16 @@ Portfolio.propTypes = {
 
 Portfolio.defaultProps = {
   siteTitle: ``,
+  portfolio01:{
+    title:'-Neighborhood.',
+    desc:`Levarging the power of google maps and foursquare 
+    to show the most interesting areas in my neightborhood.
+    User have the ability to search and learn about this places.`,
+    tags:['knockout', 'googleapi', 'foursquare'],
+    source:'https://github.com/letorruella/neighborhood'
+  },
+    
+
 }
 
 export default Portfolio
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,36 +4,50 @@ import React from "react"
 import Layout from "../components/layout"
 // import Image from "../components/image"
 import SEO from "../components/seo"
-import Portfolio from '../components/portfolio'
+import Portfolio from "../components/portfolio"
 import { Link } from "gatsby"
-
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" keywords={[`web developer`, `django developer`,'wordpress','vue', `react`]} />
-  <div>
-  <nav class="nav">
-        <Link class="link" to="/resume"> Resume. </Link>
-        <br/>
-        <a>Portfolio.</a>
-        <br/>
-        <a>Blog.</a>
-  </nav>
-    <div class="main">
-      
-      <div class="main__wrapper">
-        <h1>Luis  Torruella.</h1>
-        <p> I help companies make the best products in the web. </p>
-      </div>
-      <hr/> 
-    </div>
-    
+    <SEO
+      title=":)"
+      keywords={[
+        `web developer`,
+        `django developer`,
+        "wordpress",
+        "vue",
+        `react`,
+      ]}
+    />
+    <div>
+      <nav class="nav">
+        <Link class="link" to="/resume">
+          {" "}
+          Resume.{" "}
+        </Link>
+        <br />
+        <Link class="link" to="/resume">
+          {" "}
+          Portfolio.{" "}
+        </Link>
+        <br />
+        <Link class="link" to="/resume">
+          {" "}
+          Blog.{" "}
+        </Link>
+      </nav>
+      <div class="main">
+        <div class="main__wrapper">
+          <h1>Luis Torruella.</h1>
+          <p> I help companies make the best products in the web. </p>
         
-   <Portfolio/>
-       
+        </div>
+        <hr/>
+      </div>
+      
 
-  </div> 
-
+      <Portfolio />
+    </div>
   </Layout>
 )
 
